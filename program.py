c60f01de8cfbdb74e1c26c069f5d1f94330659b2
Goo-DarTimejar <hosein.sk123@gmail.com>
2025-05-20 13:50:08 +0330

    honing & cleaning program.py

--- program.py
+++ program.py
@@ -5,9 +5,23 @@ from subtitle_search import SubtitleSearch
 
 
 def main():
-    pass
+    prompt = Prompt()
+    while True:
+        prompt.print_help()
+        args = get_args()
+        do_as_told(args)
+        if args["quit"] or args["start"]:
+            break
+    
+    if fit_for_output(args):
+        output(args)
+        report(args)
+    else:
+        report(args)
 
-def get_args() -> Prompt:
+    finish()
+
+def get_args() -> dict:
     # The user
     # Use Argparse to parse the arguments into a Prompt
     pass
@@ -19,4 +33,34 @@ def output(args: Prompt):
     pass
 
 def get_videos():
+    pass
+
+def do_as_told(args: dict):
+    pass
+
+def fit_for_output(args: dict) -> bool:
+    # Check if the args are fit for output
+    # Check if the args are valid
+    # Check if the args are not empty
+    # Check if the args are not None
+    # Check if the args are not False
+    # Check if the args are not 0
+    # Check if the args are not ""
+    # Check if the args are not []
+    # Check if the args are not {}
+    pass
+
+def finish():
+    # Finish the program
+    # Close all the files
+    # Close all the connections
+    # Close all the threads
+    # Close all the processes
+    # Close all the sockets
+    # Close all the pipes
+    # Close all the queues
+    # Close all the locks
+    # Close all the semaphores
+    # Close all the events
+    # Close all the timers
     pass
