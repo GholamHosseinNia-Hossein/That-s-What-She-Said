5be4c701879ccb1f060e39c2f99080af10d95ec9
Goo-DarTimejar <hosein.sk123@gmail.com>
2025-05-20 11:04:43 +0330

    draw basic outline of program

---
+++ program.py
@@ -0,0 +1,22 @@
+from video import Video
+from prompt import Prompt
+from video_handler import VideoHandler
+from subtitle_search import SubtitleSearch
+
+
+def main():
+    pass
+
+def get_args() -> Prompt:
+    # The user
+    # Use Argparse to parse the arguments into a Prompt
+    pass
+
+def report(videos: list[Video], args: Prompt):
+    pass
+
+def output(args: Prompt):
+    pass
+
+def get_videos():
+    pass
