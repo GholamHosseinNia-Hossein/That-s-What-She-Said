a269bfc0bf64d0cf8429557d6a66df0626314ca5
GholamHosseinNia-Hossein <hosein.sk123@gmail.com>
2025-09-19 16:24:03 +0330

    Initialize terminal prompts through 'argparse'

--- prompt.py
+++ prompt.py
@@ -14,18 +14,24 @@ class Prompt:
         # Initializes the argument parser
         # Sets the description and usage of the program
         parser = argparse.ArgumentParser(description="Prompt for the user")
-        parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output")
-        parser.add_argument("-q", "--quiet", action="store_true", help="Enable quiet output")
-        parser.add_argument("-s", "--start", action="store_true", help="Start the clipping procedure")
-        parser.add_argument("-e", "--end", action="store_true", help="End the program")
+        parser.add_argument("-s", "--start", action="store_true", type= bool, help="Start the clipping procedure")
+        parser.add_argument("-e", "--end", action="store_true", type= bool, help="End the program")
+        parser.add_argument("--use_regex", action="store_true", type= bool, help="Shall we use 'regex' to search for the desired phrase?")
+        parser.add_argument("--directory", help="Where I searech for subtitles")
+        parser.add_argument("--save_at", help="Only the saving directory. Do not include file name!")
+        parser.add_argument("--file_name", help="Only the saved file's name. Do not include directory")
+        parser.add_argument("--lang", choices=["en", "fa", "fr"], help= "Choose a language: en, fr, fa")
+        parser.add_argument("--phrase", help="What to look for")
+
         return parser
 
     # Public
     # --------------------------------
 
-    def parse_args() -> None:
+    def parse_args() -> dict:
         # Receives arguments from the command line & Parses them
         # Updates the __args variable
+        # Returns the recieved data
         pass
 
     def print_help(self) -> None:
